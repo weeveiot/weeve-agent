346be4e0a314f74d282d5a9d575204b18d6e4f31
MarcusJones <jones.0bj3@gmail.com>
2020-10-20 16:09:48 +0200

    add header error checking in /pipeline

--- internal/controller/pipeline.go
+++ internal/controller/pipeline.go
@@ -2,7 +2,11 @@ package controller
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/golang/gddo/httputil/header"
 )
 
 //TODO: Add the code for instantiating a pipeline in the node:
@@ -12,5 +16,27 @@ import (
 //		ELSE: Continue
 // 4) Run the container
 func BuildPipeline(w http.ResponseWriter, r *http.Request) {
-	log.Debug("asdf")
+	log.Info("POST /pipeline")
+	// If the Content-Type header is present, check that it has the value
+	// application/json. Note that we are using the gddo/httputil/header
+	// package to parse and extract the value here, so the check works
+	// even if the client includes additional charset or boundary
+	// information in the header.
+	if r.Header.Get("Content-Type") != "" {
+		value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
+		if value != "application/json" {
+			msg := "Content-Type header is not application/json"
+			http.Error(w, msg, http.StatusUnsupportedMediaType)
+			return
+		}
+	}
+	// err := json.NewDecoder(r.Body).Decode(&p)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+	vars := mux.Vars(r)
+	log.Debug(vars)
+	// key := vars["id"]
+	// image := dao.DeleteData(key)
 }
