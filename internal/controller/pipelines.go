65e4f67ef6c34b13a5e10dd78360e2a7f8d4ac9d
Shri <shrikant@klientinformatics.com>
2020-10-24 21:54:33 +0530

    move code into functions (code refactoring)

--- internal/controller/pipelines.go
+++ internal/controller/pipelines.go
@@ -4,26 +4,23 @@ import (
 	"errors"
 	"net/http"
 
-	// "github.com/bitly/go-simplejson"
-
 	log "github.com/sirupsen/logrus"
 	"gitlab.com/weeve/edge-server/edge-pipeline-service/internal/docker"
 	"gitlab.com/weeve/edge-server/edge-pipeline-service/internal/model"
 	"gitlab.com/weeve/edge-server/edge-pipeline-service/internal/util"
 )
 
-//TODO: Add the code for instantiating a pipeline in the node:
+// PostPipelines function to,
 // 1) Receive manifest
 // 2) Iterate over each image
 // 3) IF image not existing locally, PULL
 //		ELSE: Continue
 // 4) Run the container
-func POST_pipelines(w http.ResponseWriter, r *http.Request) {
+func PostPipelines(w http.ResponseWriter, r *http.Request) {
 	log.Info("POST /pipeline")
 
 	// Decode the JSON manifest into Golang struct
 	manifest := model.ManifestReq{}
-
 	err := util.DecodeJSONBody(w, r, &manifest)
 	if err != nil {
 		var mr *util.MalformedRequest
@@ -37,29 +34,9 @@ func POST_pipelines(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// log.Info(w, "Person: %+v", manifest)
-
-	log.Debug("Recieved manifest: ", manifest.Name)
-	log.Debug("Number of modules in manifest: ", len(manifest.Modules))
-
-	// Iterate over the modules inside the manifest
 	// Pull all images as required
 	log.Debug("Iterate modules, Docker Pull into host if missing")
-	imagesPulled := true
-	for i := range manifest.Modules {
-		mod := manifest.Modules[i]
-		log.Debug("\tModule: ", mod.ImageName)
-		exists := docker.ImageExists(mod.ImageName)
-		log.Debug("\tImage exists: ", exists)
-
-		if exists == false {
-			// Logic for pulling the image
-			exists = docker.PullImage(mod.ImageName)
-			if exists == false {
-				imagesPulled = false
-			}
-		}
-	}
+	imagesPulled := PullImages(manifest)
 
 	// Check if all images pulled, else return
 	if imagesPulled == false {
@@ -69,36 +46,9 @@ func POST_pipelines(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Info("Pulled all images")
-
-	// Iterate over the modules inside the manifest
-	// Pull all images as required
-	log.Debug("Iterate modules, check if containers exist")
-	for i := range manifest.Modules {
-		mod := manifest.Modules[i]
-		log.Debug("\tModule: ", mod.ImageName)
-
-		// Build container name
-		containerName := GetContainerName(manifest.ID, mod.Name)
-		log.Info(containerName)
-
-		// Check if container already exists
-		containerExists := docker.ContainerExists(containerName)
-		log.Info(containerExists)
-
-		// Create container if not exists
-		if containerExists {
-			// Stop and delete container
-			err = docker.StopAndRemoveContainer(containerName)
-			if err != nil {
-				log.Info(err)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-		} else {
-			// Create and start container
-			docker.CreateContainer(containerName, mod.ImageName)
-		}
-	}
+	// Create and start containers
+	log.Debug("Iterate modules, check if containers exist, and create and start containers")
+	CreateStartContainers(manifest)
 
 	// Start all containers iteratively
 	log.Debug("Iterate modules, start each container")
@@ -123,9 +73,65 @@ func POST_pipelines(w http.ResponseWriter, r *http.Request) {
 	// Finally, return 200
 	// Return payload: pipeline started / list of container IDs
 
+	msg := "Unable to pull all images"
+	log.Debug(msg)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("200 - Request processed successfully!"))
+	return
 }
 
-// GetContainerName Build container name
-func GetContainerName(pipelineId string, containerName string) string {
-	return pipelineId + "_" + containerName
+// CreateStartContainers iterates modules, and creates and starts containers
+func CreateStartContainers(manifest model.ManifestReq) bool {
+	for i := range manifest.Modules {
+		mod := manifest.Modules[i]
+		log.Debug("\tModule: ", mod.ImageName)
+
+		// Build container name
+		containerName := GetContainerName(manifest.ID, mod.Name)
+		log.Info(containerName)
+
+		// Check if container already exists
+		containerExists := docker.ContainerExists(containerName)
+		log.Info(containerExists)
+
+		// Create container if not exists
+		if containerExists {
+			// Stop and delete container
+			err := docker.StopAndRemoveContainer(containerName)
+			if err != nil {
+				return false
+			}
+		}
+
+		// Create and start container
+		docker.CreateContainer(containerName, mod.ImageName)
+	}
+
+	return true
+}
+
+// PullImages iterates modules and pulls images
+func PullImages(manifest model.ManifestReq) bool {
+	for i := range manifest.Modules {
+		mod := manifest.Modules[i]
+		log.Debug("\tModule: ", mod.ImageName)
+		// Check if image exist in local
+		exists := docker.ImageExists(mod.ImageName)
+		log.Debug("\tImage exists: ", exists)
+
+		if exists == false {
+			// Pull image if not exist in local
+			exists = docker.PullImage(mod.ImageName)
+			if exists == false {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+// GetContainerName build container name
+func GetContainerName(pipelineID string, containerName string) string {
+	return pipelineID + "_" + containerName
 }
