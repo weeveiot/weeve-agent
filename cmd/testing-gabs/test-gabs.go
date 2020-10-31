85ff2b37128fa72bd7a6fcea324643f6efc5b54b
MarcusJones <jones.0bj3@gmail.com>
2020-10-31 10:19:32 +0100

    fix json appending from container handler

--- cmd/testing-gabs/test-gabs.go
+++ cmd/testing-gabs/test-gabs.go
@@ -9,24 +9,41 @@ import (
 
 
 func main() {
-// jsonParsed, err := gabs.ParseJSON([]byte(`{"array":["first","second","third"]}`))
-// if err != nil {
-// 	panic(err)
-// }
+	jsonParsed1, err := gabs.ParseJSON([]byte(`{"array":["first","second","third"]}`))
+	if err != nil {
+		panic(err)
+	}
 
-// for _, child := range jsonParsed.S("array").Children() {
-// 	fmt.Println(child.Data().(string))
-// }
+	for _, child := range jsonParsed1.S("array").Children() {
+		fmt.Println(child.Data().(string))
+	}
 
+	fmt.Println("ARRAY OF STRINGS")
+	jsonParsed2, err := gabs.ParseJSON([]byte(`["first","second","third"]`))
 
-jsonParsed, err := gabs.ParseJSON([]byte(`["first","second","third"]`))
+	if err != nil {
+		panic(err)
+	}
 
-if err != nil {
-	panic(err)
-}
+	for _, child := range jsonParsed2.Children() {
+		fmt.Println(child.Data().(string))
+	}
 
-for _, child := range jsonParsed.Children() {
-	fmt.Println(child.Data().(string))
-}
+	jsonParsed3, err := gabs.ParseJSON([]byte(`[{"arg":"InBroker", "val":"localhost:1883"},
+	{"arg":"ProcessName", "val":"container-1"},
+	{"arg":"InTopic", "val":"topic/source"},
+	{"arg":"InClient", "val":"weevenetwork/go-mqtt-gobot"},
+	{"arg":"OutBroker", "val":"localhost:1883"},
+	{"arg":"OutTopic", "val":"topic/c2"},
+	{"arg":"OutClient", "val":"weevenetwork/go-mqtt-gobot"}]`))
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, child := range jsonParsed3.Children() {
+		fmt.Println(child.Data().(string))
+	}
+
 
 }
--- internal/controller/containers.go
+++ internal/controller/containers.go
@@ -50,7 +50,7 @@ func GETcontainers(w http.ResponseWriter, r *http.Request) {
 	log.Debug(len(containers), " containers found")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("200 - Request processed successfully!"))
+	// w.Write([]byte("200 - Request processed successfully!"))
 	json.NewEncoder(w).Encode(containers)
 }
 
