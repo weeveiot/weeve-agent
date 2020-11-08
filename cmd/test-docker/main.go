e1121b4b3bc0d9f449458d940018517700cba92f
MarcusJones <jones.0bj3@gmail.com>
2020-11-08 17:36:35 +0100

    add restart policy

--- cmd/test-docker/main.go
+++ cmd/test-docker/main.go
@@ -15,8 +15,18 @@ import (
 func main( ) {
 	log.SetLevel(log.DebugLevel)
 	networkName := "bridge-net-test"
+
 	imageName1 := "weevenetwork/mosquitto_broker"
 	containerName1 := "mosquitto_broker"
+	var EntryPointArgs1 []string
+
+	imageName2 := "weevenetwork/mosquitto_sub"
+	containerName2 := "mosquitto_sub"
+	EntryPointArgs2 := []string{"-h mosquitto_broker", "-p 1883", "-t hello/hello"}
+
+	imageName3 := "weevenetwork/mosquitto_pub"
+	containerName3 := "mosquitto_pub"
+	EntryPointArgs3 := []string{"-h mosquitto_broker", "-p 1883", "-t hello/hello"}
 
 
 	// DOCKER CLIENT //////////
@@ -44,14 +54,13 @@ func main( ) {
 	///////////////////////////////////////
 	// CREATE AND ATTACH CONTAINER 1 //////
 	///////////////////////////////////////
-
 	// Config
 	containerConfig := &container.Config{
 		Image:        imageName1,
 		AttachStdin:  false,
 		AttachStdout: false,
 		AttachStderr: false,
-		Cmd:          nil,
+		Cmd:          EntryPointArgs1,
 		Tty:          false,
 		ExposedPorts: nil,
 	}
@@ -76,7 +85,7 @@ func main( ) {
 	}
 	log.Debug("Created container " + containerName1)
 
-	// START CONTAINER /////
+	// Start container
 	err = cli.ContainerStart(ctx, containerCreateResponse.ID, types.ContainerStartOptions{})
 	if err != nil {
 		log.Error(err)
@@ -84,7 +93,7 @@ func main( ) {
 	}
 	log.Debug("Started container")
 
-	// ATTACH TO NETWORK
+	// Attach to network
 	var netConfig network.EndpointSettings
 	err = cli.NetworkConnect(ctx, networkName, containerCreateResponse.ID, &netConfig)
 	if err != nil {
@@ -92,4 +101,111 @@ func main( ) {
 	}
 	log.Debug("Connected ", resp.ID, "to network", networkName)
 
+
+
+	///////////////////////////////////////
+	// CREATE AND ATTACH CONTAINER 2 //////
+	///////////////////////////////////////
+	// Config
+	containerConfig = &container.Config{
+		Image:        imageName2,
+		AttachStdin:  false,
+		AttachStdout: false,
+		AttachStderr: false,
+		Cmd:          EntryPointArgs2,
+		Tty:          false,
+		ExposedPorts: nil,
+	}
+
+	hostConfig = &container.HostConfig{
+		PortBindings: nil,
+		NetworkMode: "bridge",
+	}
+
+	networkConfig = &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{},
+	}
+	// Create it
+	containerCreateResponse, err = cli.ContainerCreate(ctx,
+		containerConfig,
+		hostConfig,
+		networkConfig,
+		nil,
+		containerName2)
+	if err != nil {
+		panic(err)
+	}
+	log.Debug("Created container " + containerName1)
+
+	// Start container
+	err = cli.ContainerStart(ctx, containerCreateResponse.ID, types.ContainerStartOptions{})
+	if err != nil {
+		log.Error(err)
+		panic("Failed to start container")
+	}
+	log.Debug("Started container 2")
+
+	// Attach to network
+	// var netConfig network.EndpointSettings
+	err = cli.NetworkConnect(ctx, networkName, containerCreateResponse.ID, &netConfig)
+	if err != nil {
+		panic(err)
+	}
+	log.Debug("Connected ", resp.ID, "to network", networkName)
+
+
+
+	///////////////////////////////////////
+	// CREATE AND ATTACH CONTAINER 3 //////
+	///////////////////////////////////////
+	// Config
+	containerConfig = &container.Config{
+		Image:        imageName3,
+		AttachStdin:  false,
+		AttachStdout: false,
+		AttachStderr: false,
+		Cmd:          EntryPointArgs3,
+		Tty:          false,
+		ExposedPorts: nil,
+	}
+
+	hostConfig = &container.HostConfig{
+		PortBindings: nil,
+		NetworkMode: "bridge",
+		RestartPolicy: container.RestartPolicy{
+			Name: "simple",
+			MaximumRetryCount: 100,
+		}
+	}
+
+	networkConfig = &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{},
+	}
+	// Create it
+	containerCreateResponse, err = cli.ContainerCreate(ctx,
+		containerConfig,
+		hostConfig,
+		networkConfig,
+		nil,
+		containerName3)
+	if err != nil {
+		panic(err)
+	}
+	log.Debug("Created container " + containerName1)
+
+	// Start container
+	err = cli.ContainerStart(ctx, containerCreateResponse.ID, types.ContainerStartOptions{})
+	if err != nil {
+		log.Error(err)
+		panic("Failed to start container")
+	}
+	log.Debug("Started container 3")
+
+	// Attach to network
+	// var netConfig network.EndpointSettings
+	err = cli.NetworkConnect(ctx, networkName, containerCreateResponse.ID, &netConfig)
+	if err != nil {
+		panic(err)
+	}
+	log.Debug("Connected ", resp.ID, "to network", networkName)
 }
