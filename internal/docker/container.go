ec84b1545ac32e36949abc841db8aa3892907bec
MarcusJones <jones.0bj3@gmail.com>
2020-11-08 20:02:21 +0100

    refactor old code moving out

--- internal/docker/container.go
+++ internal/docker/container.go
@@ -3,8 +3,6 @@ package docker
 
 import (
 	"fmt"
-	"io"
-	"os"
 
 	"bytes"
 	"context"
@@ -13,7 +11,6 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
 	log "github.com/sirupsen/logrus"
 	"gitlab.com/weeve/edge-server/edge-pipeline-service/internal/model"
 	"gitlab.com/weeve/edge-server/edge-pipeline-service/internal/util"
@@ -93,69 +90,6 @@ func StopContainers() bool {
 	return true
 }
 
-func StartContainers() bool {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		log.Error(err)
-	}
-
-	options := types.ContainerListOptions{All: true}
-	containers, err := cli.ContainerList(ctx, options)
-	if err != nil {
-		log.Error(err)
-	}
-
-	for _, container := range containers {
-		fmt.Print("Startings container ", container.ID[:10], "... ", container.State)
-		// if "State": "running"
-
-		if container.State != "running" {
-
-			if err := cli.ContainerStart(ctx, container.ID, types.ContainerStartOptions{}); err != nil {
-				log.Error(err)
-			}
-		}
-		fmt.Println("Success")
-	}
-	return true
-}
-
-func StartContainer(containerId string) bool {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		log.Error(err)
-	}
-
-	err = cli.ContainerStart(ctx, containerId, types.ContainerStartOptions{})
-	if err != nil {
-		log.Error(err)
-		return false
-	}
-
-	//TODO: Wait for containers
-	// statusCh, errCh := cli.ContainerWait(ctx, containerId, container.WaitConditionNotRunning)
-	// select {
-	// case err := <-errCh:
-	// 	if err != nil {
-	// log.Error(err)
-	// 		return false
-	// 	}
-	// case <-statusCh:
-	// }
-
-	out, err := cli.ContainerLogs(ctx, containerId, types.ContainerLogsOptions{ShowStdout: true})
-	if err != nil {
-		log.Error(err)
-		return false
-	}
-
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
-
-	return true
-}
-
 func StopContainer(containerId string) bool {
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -198,7 +132,6 @@ func CreateContainerOptsArgs(startCmd model.StartCommand, networkName string) bo
 		NetworkMode: startCmd.NetworkMode,
 	}
 
-	// resp, err := cli.ContainerCreate(ctx,
 	resp, err := cli.ContainerCreate(ctx,
 		containerConfig,
 		hostConfig,
@@ -246,76 +179,6 @@ func CreateContainerOptsArgs(startCmd model.StartCommand, networkName string) bo
 	log.Debug("Connected ", resp.ID, "to network", networkName)
 
 	return true
-}
-
-func CreateContainer(containerName string, imageName string) bool {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		log.Error(err)
-		// log.Error(err)
-		return false
-	}
-
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: imageName,
-		Cmd:   []string{"echo", "Container " + containerName + " created"},
-	}, &container.HostConfig{}, &network.NetworkingConfig{}, nil, containerName)
-	if err != nil {
-		log.Error(err)
-		// return "CreateFailed"
-		return false
-	}
-
-	if !StartContainer(resp.ID) {
-		return false
-	}
-
-	return true
-}
-
-func CreateContainer1(containerName string, imageName string) string {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		log.Error(err)
-	}
-
-	reader, err := cli.ImagePull(ctx, "docker.io/library/"+imageName, types.ImagePullOptions{})
-	if err != nil {
-		log.Error(err)
-	}
-	io.Copy(os.Stdout, reader)
-
-	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: imageName,
-		Cmd:   []string{"echo", "Container " + containerName + " created"},
-	}, &container.HostConfig{}, &network.NetworkingConfig{}, nil, containerName)
-	if err != nil {
-		log.Error(err)
-	}
-
-	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-		log.Error(err)
-	}
-
-	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-	select {
-	case err := <-errCh:
-		if err != nil {
-			log.Error(err)
-		}
-	case <-statusCh:
-	}
-
-	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-	if err != nil {
-		log.Error(err)
-	}
-
-	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
-
-	return "Container " + containerName + " created for image " + imageName
 }
 
 // StopAndRemoveContainer Stop and remove a container
