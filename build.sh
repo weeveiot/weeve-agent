ef0792e0dde9ab2174cfdeb68b62e4b9abbaaa61
madhavarajan.m@gmail.com madhavarajan.m@gmail.com <madhavarajan.m@gmail.com madhavarajan.m@gmail.com>
2022-01-05 22:00:30 +0530

    Build binaries for all os & arch

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+stage=$1
+listarchg=$(go tool dist list)
+# aix/ppc64        freebsd/amd64   linux/mipsle   openbsd/386
+# android/386      freebsd/arm     linux/ppc64    openbsd/amd64
+# android/amd64    illumos/amd64   linux/ppc64le  openbsd/arm
+# android/arm      js/wasm         linux/s390x    openbsd/arm64
+# android/arm64    linux/386       nacl/386       plan9/386
+# darwin/386       linux/amd64     nacl/amd64p32  plan9/amd64
+# darwin/amd64     linux/arm       nacl/arm       plan9/arm
+# darwin/arm       linux/arm64     netbsd/386     solaris/amd64
+# darwin/arm64     linux/mips      netbsd/amd64   windows/386
+# dragonfly/amd64  linux/mips64    netbsd/arm     windows/amd64
+# freebsd/386      linux/mips64le  netbsd/arm64   windows/arm
+
+echo $stage
+echo $listarchg
+
+for arch in ${listarchg[@]}
+do
+    # echo $arch
+    arrIN=(${arch//// })
+    echo ${arrIN[0]} ${arrIN[1]} agent_${arrIN[0]}_${arrIN[1]}
+    GOOS=${arrIN[0]} GOARCH=${arrIN[1]} go build -o agent_${arrIN[0]}_${arrIN[1]} cmd/agent/agent.go
+done
+
+# GOOS=android GOARCH=arm go build -o agent_android_arm cmd/agent/agent.go
+# GOOS=darwin GOARCH=386 go build -o agent_darwin_386 cmd/agent/agent.go
+# GOOS=darwin GOARCH=amd64 go build -o agent_darwin_amd64 cmd/agent/agent.go
+# GOOS=darwin GOARCH=arm go build -o agent_darwin_arm cmd/agent/agent.go
+# GOOS=darwin GOARCH=arm64 go build -o agent_darwin_arm64 cmd/agent/agent.go
+# GOOS=dragonfly GOARCH=amd64 go build -o agent_dragon_amd64 cmd/agent/agent.go
+# GOOS=freebsd GOARCH=386 go build -o agent_freebsd_386 cmd/agent/agent.go
+# GOOS=freebsd GOARCH=amd64 go build -o agent_freebsd_amd64 cmd/agent/agent.go
+# GOOS=freebsd GOARCH=arm go build -o agent_freebsd_arm cmd/agent/agent.go
+# GOOS=windows GOARCH=amd64 go build -o agent_win_amd64.exe cmd/agent/agent.go
+# GOOS=windows GOARCH=386 go build -o agent_win_386.exe cmd/agent/agent.go
+# GOOS=linux GOARCH=386 go build -o agent_linux_386 cmd/agent/agent.go
+# GOOS=linux GOARCH=amd64 go build -o agent_linux_amd64 cmd/agent/agent.go
+# GOOS=linux GOARCH=arm go build -o agent_linux_arm cmd/agent/agent.go
+# GOOS=linux GOARCH=arm64 go build -o agent_linux_arm64 cmd/agent/agent.go
+# GOOS=linux GOARCH=ppc64 go build -o agent_linux_ppc64 cmd/agent/agent.go
+# GOOS=linux GOARCH=ppc64le go build -o agent_linux_ppc64le cmd/agent/agent.go
+# GOOS=linux GOARCH=mips go build -o agent_linux_mips cmd/agent/agent.go
+# GOOS=linux GOARCH=mipsle go build -o agent_linux_mipsle cmd/agent/agent.go
+# GOOS=linux GOARCH=mips64 go build -o agent_linux_mips64 cmd/agent/agent.go
+# GOOS=linux GOARCH=mips64le go build -o agent_linux_mips64le cmd/agent/agent.go
+# GOOS=netbsd GOARCH=386 go build -o agent_netbsd_386 cmd/agent/agent.go
+# GOOS=netbsd GOARCH=amd64 go build -o agent_netbsd_amd64 cmd/agent/agent.go
+# GOOS=netbsd GOARCH=arm go build -o agent_netbsd_arm cmd/agent/agent.go
+# GOOS=openbsd GOARCH=386 go build -o agent_openbsd_386 cmd/agent/agent.go
+# GOOS=openbsd GOARCH=amd64 go build -o agent_openbsd_amd64 cmd/agent/agent.go
+# GOOS=openbsd GOARCH=arm go build -o agent_openbsd_arm cmd/agent/agent.go
+# GOOS=plan9 GOARCH=386 go build -o agent_plan9_386 cmd/agent/agent.go
+# GOOS=plan9 GOARCH=amd64 go build -o agent_plan9_amd64 cmd/agent/agent.go
+# GOOS=solaris GOARCH=amd64 go build -o agent_solaris_amd64 cmd/agent/agent.go
+
+aws s3 sync . s3://weeve-resource-772697371069-us-east-1/agent_binaries/$stage/
