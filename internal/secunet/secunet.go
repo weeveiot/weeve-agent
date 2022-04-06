d8f2fef1d3185a01245837f74bc320a281a30835
Paul Gaiduk <paul.gaiduk@weeve.network>
2022-04-06 11:57:05 +0200

    secunet: implemented network-related functions

--- internal/secunet/secunet.go
+++ internal/secunet/secunet.go
@@ -12,8 +12,10 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
+	"github.com/ahmetb/go-linq/v3"
 	"github.com/docker/docker/api/types"
 	log "github.com/sirupsen/logrus"
 	"github.com/weeveiot/weeve-agent/internal/model"
@@ -30,6 +32,7 @@ var edgeUrl = fmt.Sprintf("https://%s:%d/rest_api/v1", host, port)
 var client http.Client
 var existingImagesIdToName = make(map[string]string)
 var existingImagesNameToId = make(map[string]string)
+var existingNetworks = make(map[string]string)
 
 func init() {
 	certFile := "/var/hostdir/clientcert/cert.pem"
@@ -373,14 +376,131 @@ func ImageRemove(imageID string) error {
 
 // File: network.go
 
+// Network name constraints
+const manifestNamelength = 11
+const indexLength = 3
+const maxNetworkIndex = 999
+
+func makeNetworkName(name string) string {
+	format := "%s_%0" + fmt.Sprint(indexLength) + "d"
+
+	// Prune the name if necessary
+	if len(name) <= 0 {
+		return ""
+	} else if len(name) > manifestNamelength {
+		name = name[:manifestNamelength]
+	}
+
+	// Get new network count
+	var newCount int
+	maxCount := getLastCreatedNetworkCount()
+	if maxCount < maxNetworkIndex {
+		newCount = maxCount + 1
+	} else {
+		newCount = getLowestAvailableNetworkCount()
+		if newCount < 0 { // no available network count found
+			log.Warning("Number of data services limit is exceeded")
+			return ""
+		}
+	}
+
+	// Generate next network name
+	networkName := fmt.Sprintf(format, name, newCount)
+
+	return strings.ReplaceAll(networkName, " ", "")
+}
+
+func readAllNetworks() []types.NetworkResource {
+	log.Debug("Docker_container -> readAllNetworks")
+
+	var networks []types.NetworkResource
+
+	for _, networkName := range existingNetworks {
+		networks = append(networks, types.NetworkResource{
+			Name: networkName,
+		})
+	}
+
+	return networks
+}
+
 func ReadDataServiceNetworks(manifestID string, version string) ([]types.NetworkResource, error) {
-	return nil, nil
+	key := manifestID + version
+	networkName := existingNetworks[key]
+
+	if networkName == "" {
+		return nil, nil
+	} else {
+		networks := []types.NetworkResource{
+			{
+				Name: networkName,
+			},
+		}
+		return networks, nil
+	}
 }
 
 func CreateNetwork(name string, labels map[string]string) (string, error) {
-	return "", nil
+	networkName := makeNetworkName(name)
+	if networkName == "" {
+		log.Error("Failed to generate Network Name")
+		return "", errors.New("failed to generate network name")
+	}
+
+	key := labels["manifestID"] + labels["version"]
+	existingNetworks[key] = networkName
+	return networkName, nil
 }
 
 func NetworkPrune(manifestID string, version string) error {
+	key := manifestID + version
+	delete(existingNetworks, key)
 	return nil
+}
+
+func getLastCreatedNetworkCount() int {
+	maxCount := 0
+
+	counts := getExistingNetworkCounts()
+
+	for _, e := range counts {
+		if e > maxCount {
+			maxCount = e
+		}
+	}
+
+	return maxCount
+}
+
+func getLowestAvailableNetworkCount() int {
+	counts := getExistingNetworkCounts()
+
+	// find lowest available network count
+	for minAvailCount := 0; minAvailCount < maxNetworkIndex; minAvailCount++ {
+		available := true
+		for _, existingCount := range counts {
+			if minAvailCount == existingCount {
+				available = false
+				break
+			}
+		}
+		if available {
+			return minAvailCount
+		}
+	}
+
+	// no available count found
+	return -1
+}
+
+func getExistingNetworkCounts() []int {
+	var counts []int
+	networks := readAllNetworks()
+	linq.From(networks).Select(func(c interface{}) interface{} {
+		nm := c.(types.NetworkResource).Name
+		nm = nm[len(nm)-indexLength:]
+		count, _ := strconv.Atoi(nm)
+		return count
+	}).ToSlice(&counts)
+	return counts
 }
