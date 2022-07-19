7555ac7f28ed672e69d9e9da620ce30f1da7434a
nithinsaai <nithinsaii>
2022-07-19 05:17:19 -0400

    refactored uninstaller

--- weeve-agent-uninstaller.sh
+++ weeve-agent-uninstaller.sh
@@ -18,35 +18,20 @@ WEEVE_AGENT_DIR="$PWD/weeve-agent"
 
 SERVICE_FILE=/lib/systemd/system/weeve-agent.service
 
-if RESULT=$(sudo systemctl stop weeve-agent 2>&1); then
-  log weeve-agent stopped
-else
-  log Error while stopping weeve-agent: "$RESULT"
-fi
-
-if RESULT=$(printf " --disconnect" >> "$SERVICE_FILE" \
-  && sudo systemctl daemon-reload 2>&1); then
-  log .service file edited to disconnect
-else
-  log Error while editing weeve-agent.service: "$RESULT"
-fi
-
-if RESULT=$(sudo systemctl start weeve-agent 2>&1); then
-  log weeve-agent is initiated to disconnect ...
-else
-  log Error while starting the weeve-agent service: "$RESULT"
-fi
-
-if RESULT=$(tail -f "$WEEVE_AGENT_DIR"/Weeve_Agent.log | sed '/weeve agent disconnected/ q' 2>&1); then
+#* NOTE: tailing will be much faster than the appearing of the lastest weeve-agent logs
+if RESULT=$(sudo systemctl stop weeve-agent \
+  && printf " --disconnect" >> "$SERVICE_FILE" \
+  && sudo systemctl daemon-reload \
+  && sudo systemctl start weeve-agent \
+  && tail -f "$WEEVE_AGENT_DIR"/Weeve_Agent.log | sed '/weeve agent disconnected/ q' 2>&1); then
   log weeve-agent disconnected
 else
-  log Error while tailing the weeve-agent logs: "$RESULT"
+  log Error while restarting weeve-agent for disconnection: "$RESULT"
 fi
 
 if [ "$OS" = "Linux" ]; then
-  if RESULT=$(systemctl is-active weeve-agent 2>&1); then
-    sudo systemctl stop weeve-agent
-    sudo systemctl daemon-reload
+  if RESULT=$(sudo systemctl stop weeve-agent \
+    && sudo systemctl daemon-reload 2>&1); then
     log weeve-agent service stopped
   else
     log weeve-agent service not running
@@ -67,4 +52,4 @@ else
   log "$WEEVE_AGENT_DIR" doesnt exists
 fi
 
-log done
+log weeve-agent uninstalled
